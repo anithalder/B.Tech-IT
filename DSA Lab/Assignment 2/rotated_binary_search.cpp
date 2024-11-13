408ebba9f2978df1643d76e55da198bfea2f3d12
Halka_Coding <87062016+anithalder@users.noreply.github.com>
2024-11-13 22:01:58 +0530

    DSA Assignment 2

--- DSA Lab/Assignment 2/rotated_binary_search.cpp
+++ DSA Lab/Assignment 2/rotated_binary_search.cpp
@@ -6,7 +6,7 @@ int binarySearch(int *, int *, int *, int *);
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6}, target = 5;
+    int arr[] = {4, 5, 6, 1, 2, 3}, target = 5;
     int start = 0, end = (sizeof(arr) / sizeof(arr[0]) - 1);
     int result = binarySearch(arr, &start, &end, &target);
     if (result == -1)
@@ -14,28 +14,59 @@ int main()
         cout << "\nElement not present in the array!\n\n";
         exit(0);
     }
-    cout << "\nThe element found at " << result << "th position\n\n";
+    cout << "\nThe element found at " << result + 1 << "th position\n\n";
 }
 
 int binarySearch(int *arr, int *start, int *end, int *target)
 {
     int mid = *start + (*end - *start) / 2;
+
     if (*start > *end)
-    {
         return -1;
-    }
     if (arr[mid] == *target)
-    {
         return mid;
-    }
-    if (arr[mid] < *target)
+
+    // if (arr[mid] < *target)
+    // {
+    //     *start = mid + 1;
+    //     return binarySearch(arr, start, end, target);
+    // }
+    // else
+    // {
+    //     *end = mid - 1;
+    //     return binarySearch(arr, start, end, target);
+    // }
+
+    if (arr[*start] <= arr[mid]) // Left sorted or not
     {
-        *start = mid + 1;
-        return binarySearch(arr, start, end, target);
+        if (arr[*start] <= *target <= arr[mid]) // Checks the target will exists in left or not
+        {
+            if (*target < arr[mid])
+            {
+                *end = mid - 1;
+                return binarySearch(arr, start, end, target);
+            }
+            else
+            {
+                *start = mid + 1;
+                return binarySearch(arr, start, end, target);
+            }
+        }
     }
-    if (arr[mid] > *target)
+    else // Right sorted
     {
-        *end = mid - 1;
-        return binarySearch(arr, start, end, target);
+        if (arr[mid] <= *target <= arr[*end]) // Checks the target will exists in right or not
+        {
+            if (*target < arr[mid])
+            {
+                *end = mid - 1;
+                return binarySearch(arr, start, end, target);
+            }
+            else
+            {
+                *start = mid + 1;
+                return binarySearch(arr, start, end, target);
+            }
+        }
     }
 }

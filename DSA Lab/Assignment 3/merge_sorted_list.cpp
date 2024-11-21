580fa6a253ff1ac7cdd09cdaf05d54cc70087fd6
anithalder <anithalder360@gmail.com>
2024-11-21 22:53:36 +0530

    DSA Assignment 3

--- DSA Lab/Assignment 3/merge_sorted_list.cpp
+++ DSA Lab/Assignment 3/merge_sorted_list.cpp
@@ -7,29 +7,33 @@ typedef struct node
     node *next;
 } node;
 
-node *head = NULL;
+node *head1 = NULL;
+node *head2 = NULL;
 
 node *createNode(int);
-void createCyclicList();
-void displayList();
+node *createList();
+node *mergeList();
+void displayList(node *);
 void freeList();
-bool checkCycle(); // Function to detect and handle cycle (to be implemented by you)
 
 int main()
 {
-    createCyclicList(); // Create a hardcoded list with a cycle
-    cout << "\nThe list has been created with a cycle.\n";
+    cout << "\nFor the 1st list\n";
+    cout << "-----------------\n";
+    head1 = createList();
 
-    if (checkCycle()) // Call the function to detect a cycle
-    {
-        cout << "\n\tIn this list cycle present!"
-             << endl;
-    }
-    else
-    {
-        cout << "\n\tIn this list cycle not present!"
-             << endl;
-    }
+    cout << "\nFor the 2nd list\n";
+    cout << "-----------------\n";
+    head2 = createList();
+
+    cout << '\n';
+
+    cout << "1st List: ";
+    displayList(head1);
+    cout << "2nd List: ";
+    displayList(head2);
+    cout << "Merge List: ";
+    displayList(mergeList());
 
     freeList(); // Cleanup
     cout << "Exiting...\n";
@@ -46,52 +50,105 @@ node *createNode(int data)
     return newNode;
 }
 
-void createCyclicList()
+node *createList()
 {
-    // Create nodes
-    node *node1 = createNode(1);
-    node *node2 = createNode(2);
-    node *node3 = createNode(3);
-    node *node4 = createNode(4);
-    node *node5 = createNode(5);
+    int n, value;
+    cout << "Enter the number of nodes: ";
+    cin >> n;
 
-    // Link nodes to form a list
-    head = node1;
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
+    if (n <= 0)
+    {
+        cout << "List size must be greater than 0.\n";
+        return NULL;
+    }
 
-    // Create a cycle (node5 points back to node2)
-    node5->next = NULL;
+    // Create the head node
+    cout << "Enter value for node 1: ";
+    cin >> value;
+    node *head = createNode(value);
+    node *current = head;
+
+    // Create the remaining nodes
+    for (int i = 2; i <= n; ++i)
+    {
+        cout << "Enter value for node " << i << ": ";
+        cin >> value;
+        current->next = createNode(value);
+        current = current->next;
+    }
+
+    return head;
 }
 
 void freeList()
 {
     node *temp;
-    while (head != nullptr)
+    while (head1 != nullptr)
     {
-        temp = head;
-        head = head->next;
+        temp = head1;
+        head1 = head1->next;
+        delete temp;
+    }
+    while (head2 != nullptr)
+    {
+        temp = head2;
+        head2 = head2->next;
         delete temp;
     }
     cout << "\nAll nodes freed." << endl;
 }
 
-// Placeholder function for cycle detection
-bool checkCycle()
+void displayList(node *head)
 {
-    node *turtle = head, *rabbit = head;
-
-    while (rabbit != NULL && rabbit->next != NULL)
+    if (!head)
     {
-        turtle = turtle->next;
-        rabbit = rabbit->next->next;
-
-        if (turtle == rabbit)
-        {
-            return true;
-        }
+        cout << "Empty!\n"
+             << endl;
+        return;
     }
-    return false;
+    node *p = head;
+    while (p != NULL)
+    {
+        cout << p->data << "->";
+        p = p->next;
+    }
+    cout << "NULL\n"
+         << endl;
+}
+
+node *mergeList()
+{
+    node *p1 = head1, *p2 = head2;
+    node *dummy = new node(), *p3 = dummy;
+
+    while (p1 != NULL && p2 != NULL)
+    {
+        if (p1->data < p2->data)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+        }
+        else
+        {
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+
+    while (p1 != NULL)
+    {
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    while (p2 != NULL)
+    {
+        p3->next = p2;
+        p2 = p2->next;
+        p3 = p3->next;
+    }
+
+    return dummy->next;
 }

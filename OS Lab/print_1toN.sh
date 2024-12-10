694ce521dc7b5bd98e9e53b02bf36fed25d7fd2c
anithalder <anithalder360@gmail.com>
2024-12-10 22:55:23 +0530

    printing N natural numbers

--- OS Lab/print_1toN.sh
+++ OS Lab/print_1toN.sh
@@ -2,6 +2,7 @@ read -p "Enter the limit: " num
 
 echo -n "The natural numbers from 1 to $num is: "
 
-for ((i = 1; i < num; i++)); do
+for ((i = 1; i <= num; i++)); do
     echo -n "$i "
 done
+echo

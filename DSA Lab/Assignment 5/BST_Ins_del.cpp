3e06452283ea2163ac752e01a199d84face5d4a8
anithalder <anithalder360@gmail.com>
2024-12-10 22:40:59 +0530

    DSA Assignment 5

--- DSA Lab/Assignment 5/BST_Ins_del.cpp
+++ DSA Lab/Assignment 5/BST_Ins_del.cpp
@@ -16,16 +16,18 @@ typedef struct Node
 
 Node *root = NULL;
 
-void printTree(Node *, int);
 void createBST();
 Node *createNode(int);
 void freeTree(Node *root);
-void insertNode(Node *, int);
-void deleteNode(Node *, int);
+void printTree(Node *, int);
+void insertBST(Node *&, int);
+Node *deleteNodeBST(Node *, int);
+Node *inOrderPredecessor(Node *ptr);
 
 int main()
 {
-    // createBST();
+    createBST(); // Initialize the BST with some predefined values.
+
     while (1)
     {
         cout << "\nMenu:\n";
@@ -42,17 +44,16 @@ int main()
         case 1:
             cout << "Enter value to insert: ";
             cin >> value;
-            insertNode(root, value);
+            insertBST(root, value);
             break;
         case 2:
-            cout << "Enter value to insert: ";
+            cout << "Enter value to delete: ";
             cin >> value;
-            deleteNode(value);
+            root = deleteNodeBST(root, value);
             break;
         case 0:
             freeTree(root);
-            cout << "\nExiting...\n"
-                 << endl;
+            cout << "\nExiting...\n\n";
             exit(0);
         default:
             cout << "Invalid choice. Try again." << endl;
@@ -62,26 +63,70 @@ int main()
     return 0;
 }
 
-void insertNode(Node *root, int value)
+void insertBST(Node *&ptr, int value)
 {
-    if (root == NULL)
+    if (ptr == NULL)
     {
-        root = createNode(value);
+        ptr = createNode(value);
         return;
     }
 
-    while (root != NULL)
-    {
-        if (value == root->data)
-        {
-            return;
-            cout << "\n\tDuplication is not allowed!\n\n";
-        }
-    }
+    if (value < ptr->data)
+        insertBST(ptr->left, value);
+
+    else if (value > ptr->data)
+        insertBST(ptr->right, value);
+
+    else
+        cout << "\nDuplication is not allowed! " << value << " already exists.\n";
 }
 
-void deleteNode(int value)
+Node *deleteNodeBST(Node *ptr, int value)
 {
+    if (ptr == NULL)
+    {
+        cout << "Value " << value << " not found in the tree.\n";
+        return NULL;
+    }
+
+    if (value < ptr->data)
+    {
+        ptr->left = deleteNodeBST(ptr->left, value);
+    }
+    else if (value > ptr->data)
+    {
+        ptr->right = deleteNodeBST(ptr->right, value);
+    }
+    else
+    {
+        // Node with one child or no child
+        if (ptr->left == NULL)
+        {
+            Node *temp = ptr->right;
+            delete ptr;
+            return temp;
+        }
+        else if (ptr->right == NULL)
+        {
+            Node *temp = ptr->left;
+            delete ptr;
+            return temp;
+        }
+
+        // Node with two children
+        Node *inPre = inOrderPredecessor(ptr);
+        ptr->data = inPre->data;
+        ptr->left = deleteNodeBST(ptr->left, inPre->data);
+    }
+    return ptr;
+}
+
+Node *inOrderPredecessor(Node *ptr)
+{
+    ptr = ptr->left;
+    while (ptr->right != NULL)
+        ptr = ptr->right;
+    return ptr;
 }
 
 Node *createNode(int data)
@@ -92,25 +137,20 @@ Node *createNode(int data)
 // Function to print the BST
 void printTree(Node *root, int space = 0)
 {
-    // Base case: if the node is null, return
     if (root == nullptr)
         return;
 
-    // Increase distance between levels
     space += 5;
 
-    // Process right child first (to print it above the current node)
     printTree(root->right, space);
 
-    // Print current node after space
     cout << '\n';
     for (int i = 5; i < space; i++)
     {
-        cout << ' '; // Print spaces for indentation
+        cout << ' ';
     }
-    cout << root->data; // Print the node's data
+    cout << root->data;
 
-    // Process left child
     printTree(root->left, space);
 }
 
@@ -120,46 +160,24 @@ void freeTree(Node *root)
     if (root == nullptr)
         return;
 
-    // Recursively free the left and right subtrees
     freeTree(root->left);
     freeTree(root->right);
 
-    // Delete the current node
     delete root;
 }
 
-// Let's consider the BST Look's like this
-/*
-              8
-             / \
-            3  10
-           / \  \
-          1  7  14
-*/
-/*
+// Function to create a predefined BST
 void createBST()
 {
-    // creating the nodes
+    root = NULL; // Reset root for initialization
 
-    Node *node1 = createNode(8);
-    Node *node2 = createNode(3);
-    Node *node3 = createNode(10);
-    Node *node4 = createNode(1);
-    Node *node5 = createNode(7);
-    Node *node6 = createNode(14);
-
-    // creating the BST
-
-    node1->left = node2;
-    node1->right = node3;
-
-    node2->left = node4;
-    node2->right = node5;
-
-    node3->left = node6;
-
-    // initialization of root
-
-    root = node1;
+    insertBST(root, 8);
+    insertBST(root, 3);
+    insertBST(root, 10);
+    insertBST(root, 1);
+    insertBST(root, 6);
+    insertBST(root, 14);
+    insertBST(root, 4);
+    insertBST(root, 7);
+    insertBST(root, 13);
 }
-*/

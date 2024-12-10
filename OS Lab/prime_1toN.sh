240c4b2f2d39fcc7e3cc49c057c4717ff333820a
anithalder <anithalder360@gmail.com>
2024-12-10 23:10:47 +0530

    prime numbers from given range

--- OS Lab/prime_1toN.sh
+++ OS Lab/prime_1toN.sh
@@ -1,1 +1,22 @@
+read -p "Enter the limit: " limit
 
+if ((limit <= 0)); then
+    echo -e "\n\tInvalid input\n"
+    exit
+fi
+
+echo -n "The prime numbers upto $limit are: "
+
+for ((i = 1; i <= limit; i++)); do
+    count=0
+    for ((j = 2; j <= i / 2; j++)); do
+        if ((i % j == 0)); then
+            ((count++))
+        fi
+    done
+    if ((!count)); then
+        echo -n "$i "
+    fi
+done
+
+echo

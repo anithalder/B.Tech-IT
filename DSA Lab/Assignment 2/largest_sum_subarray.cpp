a6bcece1edd4c12e9efe34c9262171a626008d51
Halka_Coding <87062016+anithalder@users.noreply.github.com>
2025-01-25 20:25:54 +0530

    modification

--- DSA Lab/Assignment 2/largest_sum_subarray.cpp
+++ DSA Lab/Assignment 2/largest_sum_subarray.cpp
@@ -11,18 +11,28 @@ int main()
     cout << "The array is: ";
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         cout << arr[i] << " ";
-    cout << "\nThe largest sum subarray is " << largestSumSubarray(arr, &size);
+    cout << endl;
+    cout << " with sum " << largestSumSubarray(arr, &size);
 }
 
 int largestSumSubarray(int *arr, int *size)
 {
     int maxSum = INT_MIN, currSum = 0;
+    int start = 0, end = 0, tempStart = 0;
     for (int i = 0; i < *size; i++)
     {
         currSum += arr[i];
         maxSum = max(currSum, maxSum);
+        start = tempStart, end = i;
         if (currSum < 0)
+        {
             currSum = 0;
+            tempStart = i + 1;
+        }
     }
+    cout << "The largest subarray is: [ ";
+    for (int i = start; i <= end; i++)
+        cout << arr[i] << " ";
+    cout << "]";
     return maxSum;
 }

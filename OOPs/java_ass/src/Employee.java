bcf06d5ea116003566ce491e8ebc7b4a201349b8
Halka_Coding <87062016+anithalder@users.noreply.github.com>
2025-03-05 20:22:00 +0530

    updated

---
+++ OOPs/java_ass/src/Employee.java
@@ -0,0 +1,9 @@
+class Employee {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Employee salary can be access by the same employee member
+// Employee name can be access by anyone
+// Employee ID can be access by another group in same company
